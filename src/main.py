520192c45b365d153c1a779fcb3fac9d5d200ac6
Dongjun Shin <dj.shin@jnpmedi.com>
2022-11-08 10:21:40 +0900

    add-debugging

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def push_proc(repo):
         pulled_branches = repo.remotes.origin.pull()
         # push 실행
         pushed_branch = repo.remotes.origin.push()
-        
+
         return 'push_proc : Success'
     except Exception as push_err:
         GitErrLog(push_err=push_err, pull_result=pulled_branches, push_result=pushed_branch)
@@ -63,6 +63,8 @@ def commit_proc(repo):
     # git staging 생성
     add_files, del_files = get_tracked_path(repo)
     r_index = repo.index
+    print(add_files, del_files)
+
     try:
         stg_add, stg_del = [], []
         if add_files:
